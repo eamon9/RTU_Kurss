22ae83119bdd2bc0a32cc1e1801cf806542a634b
eamon9 <81514128+eamon9@users.noreply.github.com>
2022-06-11 16:58:55 +0300

    Continue working on Page look and feel And add first Button to go from MainPage to LoginPage

--- src/RTU_JAVA_kurss/AllPages.java
+++ src/RTU_JAVA_kurss/AllPages.java
@@ -6,14 +6,56 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 class MainPage implements ActionListener {
+    MyFrame mainPage= new MyFrame();
+    JLabel titleLabel= new JLabel("Document Solutions");
+    JLabel subTitleLabel1= new JLabel("Mēs GLABĀJAM,");
+    JLabel subTitleLabel2= new JLabel("SMALCINAM,");
+    JLabel subTitleLabel3= new JLabel("IZNĪCINAM.");
+    JButton startBtn= new JButton("Sākt");
 
     MainPage() {
+        mainPage.setTitle("Document Solutions Main Page");
 
+        titleLabel.setForeground(new Color(11, 87, 3));
+        titleLabel.setFont(new Font("Times New Roman", Font.BOLD, 60));
+        titleLabel.setBounds(200, 100, 600, 300);
+
+        subTitleLabel1.setForeground(new Color(7, 105, 64, 190));
+        subTitleLabel1.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        subTitleLabel1.setBounds(250, 270, 250, 50);
+
+        subTitleLabel2.setForeground(new Color(7, 105, 64, 190));
+        subTitleLabel2.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        subTitleLabel2.setBounds(400, 290, 250, 50);
+
+        subTitleLabel3.setForeground(new Color(7, 105, 64, 190));
+        subTitleLabel3.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        subTitleLabel3.setBounds(520, 310, 250, 50);
+
+        startBtn.setBounds(350, 450, 200, 50);
+        startBtn.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        startBtn.setOpaque(true);
+        startBtn.setBorderPainted(false);
+        startBtn.setBackground(new Color(184, 229, 154));
+        startBtn.setFocusable(false);
+        startBtn.addActionListener(this);
+
+
+        mainPage.add(startBtn);
+        mainPage.add(subTitleLabel3);
+        mainPage.add(subTitleLabel2);
+        mainPage.add(subTitleLabel1);
+        mainPage.add(titleLabel);
+        mainPage.setLayout(null);
+        mainPage.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if(e.getSource().equals(startBtn)) {
+            mainPage.dispose();
+            new LoginPage();
+        }
     }
 }
 
--- src/RTU_JAVA_kurss/Main.java
+++ src/RTU_JAVA_kurss/Main.java
@@ -3,6 +3,7 @@ package RTU_JAVA_kurss;
 public class Main {
 
     public static void main(String[] args) {
-        new LoginPage();
+        new MainPage();
+        //new LoginPage();
     }
 }
